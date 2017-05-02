167deecafb8887a5968a0dad4717cfda2c722172
Rui Carmo <rui.carmo@gmail.com>
2017-05-02 14:21:47 +0000

    moving to Sanic

--- test-service/src/app.py
+++ test-service/src/app.py
@@ -1,42 +1,113 @@
-from os import environ
-from bottle import route, view, run, static_file
-from socket import gethostname
-from time import time
-from datetime import datetime
-from random import randint, seed
+#!/usr/bin/env python3
 
+""" Web server """
+
+from logging.config import dictConfig
+from os import environ
+from datetime import datetime
+from multiprocessing import cpu_count
+from mako.template import Template
+from sanic import Sanic
+from sanic.exceptions import FileNotFound, NotFound
+from sanic.response import json, text, html
+from aioredis import create_redis
+from socket import gethostname
+from random import seed
+from logging import getLogger, DEBUG
+
+dictConfig({
+    "version": 1,
+    "formatters": {
+        "http": {
+            "format" : 'timestamp=%(asctime)s pid=%(process)d level=%(levelname)s uri=%(message)s',
+            "datefmt": "%Y-%m-%dT%H:%M:%SZ"
+        },
+        "service": {
+            "format" : 'timestamp=%(asctime)s level=%(levelname)s %(message)s',
+            "datefmt": "%Y-%m-%dT%H:%M:%SZ"
+        }
+    },
+    "handlers": {
+        "console": {
+            "class"    : "logging.StreamHandler",
+            "formatter": "service",
+            "level"    : "DEBUG",
+            "stream"   : "ext://sys.stdout"
+        }
+    },
+    "loggers": {
+        "sanic.static": {
+            "level"   : "INFO",
+            "handlers": ["console"]
+        }
+    },
+    "root": {
+        "level"   : "INFO",
+        "handlers": ["console"]
+    }
+})
+
+
+log = getLogger()
+app = Sanic(__name__)
+layout = Template(filename='views/layout.tpl')
+redis = None
+shared_counter = environ.get('REDIS_COUNTER', 'rcarmo/stateful-service')
 request_count = 0
 compute_time = 0
-seed()
 
-@route('/')
-@view('layout')
-def homepage():
+@app.route('/', methods=['GET'])
+async def homepage(req):
     global request_count
     request_count = request_count + 1
-    return {'hostname': gethostname(), 'requests': request_count, 'timestr': datetime.now().strftime("%H:%M:%S.%f")}
+    shared_count = 0
+    if redis:
+        await redis.inc(shared_counter)
+        shared_count = int(await redis.get(shared_counter))
+    return html(layout.render(request=req,
+                              hostname=gethostname(),
+                              requests=request_count,
+                              shared_count=shared_count,
+                              timestr=datetime.now().strftime("%H:%M:%S.%f")))
 
-@route('/hostname')
-def get_name():
+
+@app.route('/hostname')
+async def get_name(req):
     global request_count
     request_count = request_count + 1
-    return gethostname()
+    if redis:
+        await redis.inc(shared_counter)
+    return text(gethostname())
 
-@route('/count')
-def get_count():
+@app.route('/count')
+async def get_count(req):
     global request_count
-    return str(request_count)
+    return text(str(request_count))
 
-@route('/stats')
-def get_count():
+@app.route('/shared_count')
+async def get_count(req):
+    if redis:
+        return text(redis.get(shared_counter))
+    return text('No shared count available', status=404)
+
+@app.route('/metrics')
+async def prometheus_metrics(req):
     global request_count, compute_time
-    return "%s: %d requests, %d ms computed, time is %f" % (gethostname(), request_count, compute_time, time())
+    shared_count = 0
+    if redis:
+        shared_count = int(await redis.get(shared_counter))
+    return text('instance_compute_time %d\ninstance_requests_count %d\nshared_requests_count %d' % (compute_time, request_count, shared_count)) 
 
-@route('/reset')
-def get_count():
+@app.route('/reset')
+async def reset_count(req):
     global request_count, compute_time
     request_count = compute_time = 0
-    return "Reset."
+    return text('Instance count reset')
+
+@app.route('/reset_shared')
+async def reset_shared_count(req):
+    await redis.set(shared_counter, 0)
+    return text('Shared count reset')
 
 def make_pi(digits):
     """Compute digits of Pi"""
@@ -48,12 +119,12 @@ def make_pi(digits):
         else:
             q, r, t, k, m, x = q*k, (2*q+r)*x, t*x, k+1, (q*(7*k+2)+r*x)//(t*x), x+2
 
-@route('/compute/<a:int>,<b:int>')
-def compute_for_random_interval(a,b):
-    return compute_for_time(randint(a,b))
+@app.route('/compute/<a:int>,<b:int>')
+async def compute_for_random_interval(a,b):
+    return text(compute_for_time(randint(a,b)))
 
-@route('/compute/<ms:int>')
-def compute_for_time(ms):
+@app.route('/compute/<ms:int>')
+async def compute_for_time(ms):
     global compute_time
     result = []
     now = time()
@@ -63,16 +134,24 @@ def compute_for_time(ms):
         if time() >= limit:
             break
     compute_time = compute_time + ms
-    return "".join(result)
+    return text("".join(result))
 
-@route('<path:path>')
-def static(path):
-    return static_file(path, root='static')
+app.static('/', './static')
+
+
+@app.listener('after_server_start')
+async def init_connections(sanic, loop):
+    """Bind the database and Redis client to Sanic's event loop."""
+
+    global redis
+    redis_server = environ.get('REDIS_SERVER', None)
+    if redis_server:
+         redis = await create_redis((redis_server, 6379), encoding='utf-8', loop=loop)
+    seed()
+
 
 if __name__ == '__main__':
-    run(
-        host    = environ.get('BIND_ADDRESS','0.0.0.0'),
-        port    = int(environ.get('PORT','8000')),
-        debug   = environ.get('DEBUG','False').lower() == 'true',
-        server  = 'uvloop'
-    )
+    log.debug("Beginning run.")
+    HTTP_PORT = environ.get('PORT', 8000)
+    DEBUG = 'true' == environ.get('DEBUG', 'false').lower()
+    app.run(host='0.0.0.0', port=HTTP_PORT, workers=cpu_count(), debug=DEBUG)
--- test-service/src/views/layout.tpl
+++ test-service/src/views/layout.tpl
@@ -4,7 +4,7 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 
-	<title>{{!hostname}}</title>
+	<title>${hostname}</title>
 	<meta name="description" content="Simple stateless demo app">
 
 	<link href="minimal.css" rel="stylesheet">
@@ -12,11 +12,11 @@
 <body>
 <div class="container">
 	<header>
-		<a class="logo">Hostname: {{!hostname}}</a>
+		<a class="logo">Hostname: ${hostname}</a>
 		<nav class="float-right">
 			<ul>
-				<li>{{!requests}} requests</li>
-				<li>{{!timestr}}</li>
+				<li>${requests} local requests (${shared_count} global)</li>
+				<li>${timestr}</li>
 			</ul>
 		</nav>
 	</header>
@@ -29,8 +29,9 @@
             <ul>
                 <li><tt>/hostname</tt> to obtain the hostname (requests to this URL and to the homepage are counted)</li>
                 <li><tt>/count</tt> to get the request count</li>
-                <li><tt>/stats</tt> for more detailed stats</li>
+                <li><tt>/shared_count</tt> to get the shared request count</li>
                 <li><tt>/reset</tt> to reset request and computation counters</li>
+                <li><tt>/reset_shared</tt> to reset the shared request counter</li>
                 <li><tt>/compute/100</tt> to perform a computation during 100ms (approximately)</li>
                 <li><tt>/compute/100,200</tt> to perform a computation for a random interval between 100 and 200ms</li>
             </ul>
@@ -44,7 +45,9 @@
 	    	<h2>Server Environment</h2>
 	    </div>
         <hr>
-% from os import environ
+<%
+from os import environ
+%>
         <table class="table">
             <tr>
                 <th scope="col">Variable Name</th>
@@ -52,10 +55,10 @@
             </tr>
 % for k, v in sorted(list(environ.items())):
             <tr>
-                <th scope="row">{{!k}}</th>
-                <td>{{!v}}</td>
+                <th scope="row">${k}</th>
+                <td>${v}</td>
             </tr>
-% end
+% endfor
     	</table><!-- table -->
     </div>
     <div class="col-6">
@@ -63,18 +66,17 @@
             <h2>Request Environment</h2>
         </div>
         <hr>
-% from bottle import request
         <table class="table">
             <tr>
                 <th scope="col">Variable Name</th>
                 <th scope="col">Value</th>
             </tr>
-% for k, v in sorted(list(request.environ.items())):
+% for k, v in sorted(list(request.args.items())):
             <tr>
-                <th scope="row">{{!k}}</th>
-                <td>{{!v}}</td>
+                <th scope="row">${k}</th>
+                <td>${v}</td>
             </tr>
-% end
+% endfor
     	</table><!-- table -->
     </div>
     </div>
